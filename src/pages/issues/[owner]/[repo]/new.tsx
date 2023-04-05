44fe7ee775fc555a88d452be3a02a79c58f3aaea
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-04-05 17:17:01 +0800

    done new issue

--- src/pages/issues/[owner]/[repo]/new.tsx
+++ src/pages/issues/[owner]/[repo]/new.tsx
@@ -1,9 +1,27 @@
-import { AppShell, Container, Divider, Title } from "@mantine/core";
+import { AppShell, Container, Divider, Space, Title } from "@mantine/core";
 
 import Header from "@/components/Header";
 import SignOutButton from "@/components/SignOutButton";
+import IssueEditor from "@/components/IssueEditor";
+import { useRouter } from "next/router";
+import { type IssuesPageQuery } from ".";
+import { useSession } from "next-auth/react";
+import Redirecting from "@/components/Redirecting";
 
 export const NewIssue = () => {
+  const router = useRouter();
+  const { owner, repo } = router.query as IssuesPageQuery;
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated: () => {
+      router.push("/signin").catch(console.error);
+    },
+  });
+
+  if (status !== "authenticated") {
+    return <Redirecting />;
+  }
+
   return (
     <AppShell
       header={
@@ -14,7 +32,16 @@ export const NewIssue = () => {
     >
       <Container className="py-4">
         <Title order={2}>New Issue</Title>
+        <Title order={3} color="dimmed">
+          {owner}/{repo}
+        </Title>
         <Divider className="my-2" />
+        <Space h="xl" />
+        <IssueEditor
+          handleSubmit={({ title, body }) => {
+            console.log(title, body);
+          }}
+        />
       </Container>
     </AppShell>
   );
